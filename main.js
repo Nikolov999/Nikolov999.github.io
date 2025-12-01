672cda640d3cd4751b7e45296682b66f326b9b4c
bobo-SOC <140917342+Nikolov999@users.noreply.github.com>
2025-12-01 19:19:48 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-// -----------------------------
-// 1) Microsoft Clarity tracking
-// -----------------------------
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/YOUR_CLARITY_ID";
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "uetx3tchok");
-
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "uetx3tchok");
+</script>
 // -----------------------------
 // 2) DOM Ready
 // -----------------------------
