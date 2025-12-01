ea463f573df0e6a1e4c013b02f27cef38e86e424
bobo-SOC <140917342+Nikolov999@users.noreply.github.com>
2025-12-01 18:15:53 +0000

    Update main.js

--- main.js
+++ main.js
@@ -30,3 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
   injectFragment("nav-placeholder", "nav.html");
   injectFragment("footer-placeholder", "footer.html");
 });
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "uetx3tchok");
