706dee0e1e1759acae0dfcf585a2749f2f8c011d
bobo-SOC <140917342+Nikolov999@users.noreply.github.com>
2025-12-01 19:41:38 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // -----------------------------
 (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/YOUR_CLARITY_ID";
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/uetx3tchok";
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "uetx3tchok");
 
